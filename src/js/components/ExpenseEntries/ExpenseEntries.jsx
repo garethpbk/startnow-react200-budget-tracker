09bc3c996b24ba2a439298d3c065dc8d54d183b4
Gareth Bk <garethbk@gmail.com>
2018-03-23 15:22:33 -0700

    Finished project

--- src/js/components/ExpenseEntries/ExpenseEntries.jsx
+++ src/js/components/ExpenseEntries/ExpenseEntries.jsx
@@ -1,27 +1,64 @@
 import React, { Component } from 'react';
 
+import { updateExpenseDescription, updateExpenseAmount, addExpense } from './expenseActions';
+
 export default class ExpenseEntries extends Component {
   constructor(props) {
     super(props);
+
+    this.handleDescriptionInput = this.handleDescriptionInput.bind(this);
+    this.handleAmountInput = this.handleAmountInput.bind(this);
+    this.handleAddExpense = this.handleAddExpense.bind(this);
+  }
+
+  handleDescriptionInput(event) {
+    const { dispatch } = this.props;
+    const { value } = event.target;
+    dispatch(updateExpenseDescription(value));
+  }
+
+  handleAmountInput(event) {
+    const { dispatch } = this.props;
+    const { value } = event.target;
+    dispatch(updateExpenseAmount(value));
+  }
+
+  handleAddExpense() {
+    const { description, amount, dispatch } = this.props;
+    dispatch(addExpense(description, amount));
   }
 
   render() {
+    const { description, amount, lineItems } = this.props;
     return (
       <div className="card border-danger mb-3">
+        <div className="card-header text-white bg-danger">Expense Entries</div>
         <div className="card-body">
           <form>
             <div className="form-group">
               <label htmlFor="expense-description">Description</label>
-              <input type="text" className="form-control" id="expense-description" />
+              <input
+                type="text"
+                className="form-control"
+                id="expense-description"
+                value={description}
+                onChange={this.handleDescriptionInput}
+              />
             </div>
             <div className="form-group">
               <label htmlFor="expense-amount">Amount</label>
               <div className="input-group">
                 <span className="input-group-addon">$</span>
-                <input type="text" className="form-control" id="expense-amount" />
+                <input
+                  type="text"
+                  className="form-control"
+                  id="expense-amount"
+                  value={amount}
+                  onChange={this.handleAmountInput}
+                />
               </div>
             </div>
-            <button type="button" className="btn btn-danger col-12 mb-5">
+            <button type="button" className="btn btn-danger col-12 mb-5" onClick={this.handleAddExpense}>
               + Add Expense
             </button>
             <table className="table table-sm table-hover">
@@ -32,10 +69,12 @@ export default class ExpenseEntries extends Component {
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td>Rent</td>
-                  <td>$1,500.00</td>
-                </tr>
+                {lineItems.map(lineItem => (
+                  <tr>
+                    <td>{lineItem.description}</td>
+                    <td>${lineItem.amount.toFixed(2)}</td>
+                  </tr>
+                ))}
               </tbody>
             </table>
           </form>
