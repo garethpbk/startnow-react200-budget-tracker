fdbb8c0d5d492e9334850b8c95b656642c8ea35f
Gareth Bk <garethbk@gmail.com>
2018-03-23 15:39:13 -0700

    Fixing for heroku

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(morgan('dev'));
-app.use('/dist', express.static(__dirname + '/dist'));
-app.use('/public', express.static(__dirname + '/public'));
+app.use(express.static('dist'));
+app.use(express.static('public'));
 
 module.exports = app;
